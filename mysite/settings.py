a5ac88336292f9bc5a0cd5ddf965df2737d7661e
nagas <nagasaivignesh2005@gmail.com>
2025-12-15 15:01:35 +0530

    Changed Databases settingsin mysite/settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,14 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'potato',
-        'USER': 'postgres',
-        'PASSWORD': '8125888277',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+
+        default=os.environ.get('DATABASE_URL',
+                               'posstgres://postgres:8125888277@locahost:5432/poato')
+        )
+
 }
 
 
